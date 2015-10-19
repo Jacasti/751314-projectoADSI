139f1da65b8e378f6a0412cf6c1036343c71f904
USUARIO <USUARIO@administrador>
2015-10-19 16:26:15 -0500

    ensayo de archivos y conexion

--- model/login_class.php
+++ model/login_class.php
@@ -1,8 +1,46 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class conexion {
+    
+    
+    private $db;
+    private $usuario;
+    private $servidor ;
+    private $password ; 
+    private $conect;
+    private $encad; 
+ 
+    public function conexion (){
+
+    $this->db = "proyecto_adsi";
+    $this->usuario = "root";
+    $this->servidor = "localhost";
+    $this->password = ""; 
+    
+        
+    
+
+
+    }
+    
+    
+    public  function conectar(){
+        
+        $this->conect = mysql_connect($this->servidor, $this->usuario, $this->password) or die ("problemas con tratar de conectar con el servidor");
+        return $this->conect; 
+       
+    }
+    
+    public function seleccion_db(){
+        
+    mysql_select_db($this->db , $this->conect) or die ("problemas al tratar de conectar con la base de datos");
+    
+        
+        
+    }
+    
+  
+}
+
+?>
 
--- view/formularios/frm_login.php
+++ view/formularios/frm_login.php
@@ -1,8 +1,27 @@
-<?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+<form method="post" action="" >
+  <fieldset>
+    <legend  style="font-size: 18pt"><b>Registro</b></legend>
+    <div class="form-group">
+      <label style="font-size: 14pt"><b>Ingresa tu nombre</b></label>
+      <input type="text" name="realname" class="form-control" placeholder="Ingresa tu nombre" />
+    </div>
+    <div class="form-group">
+      <label style="font-size: 14pt; color: #FFFFFF;"><b>Ingresa tu email</b></label>
+      <input type="text" name="nick" class="form-control"  required placeholder="Ingresa mail"/>
+    </div>
+    <div class="form-group">
+      <label style="font-size: 14pt; color: #FFFFFF;"><b>Ingresa tu Password</b></label>
+      <input type="password" name="pass" class="form-control"  placeholder="Ingresa contraseña" />
+    </div>
+    <div class="form-group">
+      <label style="font-size: 14pt"><b>Repite tu contraseña</b></label>
+      <input type="password" name="rpass" class="form-control" required placeholder="repite contraseña" />
+    </div>
+      
+    </div>
+   
+    <input  class="btn btn-danger" type="submit" name="submit" value="Registrarse"/>
 
+  </fieldset>
+</form>
