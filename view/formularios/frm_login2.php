2e1cc786e0246f2304dc92068dd43a32e3ff7416
WILSON <WILSON@DESKTOP-BK1CLUK>
2015-10-20 10:52:47 -0500

    Se Hace el login

--- view/formularios/frm_login2.php
+++ view/formularios/frm_login2.php
@@ -1,8 +1,29 @@
-<?php
+<html>
+<head>
+  <meta charset="utf-8">
+     <title>Documento sin título</title>
+</head>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+ <body>
+   <h1 align="center">WELCOME</h1>
+   <h1 align="center"> Administrador</h1>
+     <p align="center">&nbsp;</p>
+     <p align="center"><img src="../Downloads/Imagenes/Ingreso.png" width="132" height="107"  alt=""/></p>
+     <p align="center">&nbsp;</p>
+     <p align="center">
+         
+  <label for="password"> User Name : </label>
+  <input type="text"  name="nombre" id="nombre">
+  </p>
+  
+     <p align="center">&nbsp;</p>
+     <p align="center">
+  <label for="password">Password:</label>
+  <input type="password" name="password" id="password">
+   </p>
+    <p align="justify">&nbsp;</p>
+    <p align="center">
+  <input type="submit" name="submit" id="submit" value="Entrar">
+   </p>
+ </body>
+</html>
