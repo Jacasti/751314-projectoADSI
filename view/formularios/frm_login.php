7c2d0e62db952322f61dc9e9bcac60c6fb9ac308
USUARIO <USUARIO@administrador>
2015-10-25 20:54:53 -0500

    se construye el formulario Login con codigo digitado

--- view/formularios/frm_login.php
+++ view/formularios/frm_login.php
@@ -1,74 +1,65 @@
+<?
+session_start();
 
-<html>
-<head>
+?>
+<!DOCTYPE html>
 
-	<meta charset="utf-8">
-		<link rel="stylesheet" href="bootstrap/css/bootstrap.css">
-		<link rel="stylesheet" href="bootstrap/css/bootstrap-responsive.css">
-		<link rel="stylesheet" type="text/css" href="estilos/estilos.css">
-	<title>Proyecto ADSI 751314</title>
-</head>
-<body background="images/golf.jpg" style="background-attachment: fixed" >
-	<center><div class="tit"><h2 style="color: #0000FF; ">Inicio de sesión</h2>
-		<center><div class="Ingreso">
-
-	<table border="0" align="center" valign="middle">
-		<tr>
-		<td rowspan=2>
-		<form action="validar.php" method="post">
-
-		<table border="0">
-
-		<tr><td><label style="font-size: 14pt"><b>Correo: </b></label></td>
-			<td width=80> <input class="form-group has-success" style="border-radius:15px;" type="text" name="mail"></td></tr>
-		<tr><td><label style="font-size: 14pt"><b>Contraseña: </b></label></td>
-			<td witdh=80><input style="border-radius:15px;" type="password" name="pass"></td></tr>
-		<tr><td></td>
-			<td width=80 align=center><input class="btn btn-primary" type="submit" value="Aceptar"></td>
-			</tr></tr></table>
-		</form>
-<br>
-<!-- formulario registro -->
-
-<form method="post" name="frmLogin" action="controller/login_controller.php?solicitud=validalogin" method="POST" autocomplete="off">
-  <fieldset>
-    <legend  style="font-size: 18pt"><b>Registro</b></legend>
-    <div class="form-group">
-      <label style="font-size: 14pt"><b>Ingresa tu nombre</b></label>
-      <input type="text" name="realname" class="form-control" placeholder="Ingresa tu nombre" />
-    </div>
-    <div class="form-group">
-      <label style="font-size: 14pt; color: #FFFFFF;"><b>Ingresa tu email</b></label>
-      <input type="text" name="nick" class="form-control"  required placeholder="Ingresa mail"/>
-    </div>
-    <div class="form-group">
-      <label style="font-size: 14pt; color: #FFFFFF;"><b>Ingresa tu Password</b></label>
-      <input type="password" name="pass" class="form-control"  placeholder="Ingresa contraseña" />
-    </div>
-    <div class="form-group">
-      <label style="font-size: 14pt"><b>Repite tu contraseña</b></label>
-      <input type="password" name="rpass" class="form-control" required placeholder="repite contraseña" />
-    </div>
-      
-    </div>
-   
-    <input  class="btn btn-danger" type="submit" name="submit" value="Registrarse"/>
-
-  </fieldset>
-</form>
-</div>
-<?php
-		if(isset($_POST['submit'])){
-			require("registro.php");
-		}
-	?>
-<!--Fin formulario registro -->
-
+<html>	
+  <head>
+     <meta charset="UTF-8">
+	<title> MI PRIMER LOGIN </title>
+       <link rel="stylesheet" href="estilo.css">
+  </head>
+<body align="center">
+    <h1 align="center">INICIA SESSION PROYECTO ADSI </h1>
+	<form>
+            <table class="fondo" align="center" id="frm-table">
+              <tr>
+		<td>
+	          <tr>
+		    <td>
+                       <nav>
+			<td cols="">Escoga una opcion :</td>
+			<td><select name="cboSeleccionRol" align="center">
+			<option value="Seleccion Su Rol O tipo de Usuario" size="25" maxlength="25">Seleccione su rol<br></option>
+			<option value="Usuario">Usuario</option>
+			<option value="Tecnico">Tecnico</option>
+			<option value="Administrador">Administrador</option>
+			<option value="Invitado">Invitado</option>
+			</select>
+			</td><br>
+			</nav>				
+			</td>			
+                    </tr><br>
+            <tr>
+                <td>
+                    <div>
+                    <td cols="">Ingrese Su Usuario:</td>
+                    <td><input type="text" class="txt-login" name="txtUsername" placeholder="Nombre de Usuario"  size="20 maxlength="25"><br></td>
+                    </div>
 		</td>
-		</tr>
-		</table>
-		</div></center></div></center>
+            </tr>
+            <tr>
+              <td>
+		<div>
+		<td cols="">Ingrese Su Contraseña:</td>
+		<td><input type="password" class="txt-login" name="txtPassword" placeholder="Su Contraseña " size="20" maxlength="25"><br></td>
+		</div><br>
+            </td>
+            </tr>
+            <tr align="right">
+              <td>
+		<td><input type="submit"  name="btnLogin" id="btn-login" value="Ingresar"></td>
+	      </td>
+	     </tr>
+	    </td>
+	   </tr>	
+	</table>	
+     </form>
+</body>
+
+
+
+</html>
 
 	
-</body>
-</html>
